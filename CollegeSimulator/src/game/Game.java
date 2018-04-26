41f46dbf02336eac25f5d1a640956dbcd46be3e0
justinyburton <justin.y.burton@gmail.com>
2018-04-26 12:03:42 -0400

    Added scrren width and units.

--- CollegeSimulator/src/game/Game.java
+++ CollegeSimulator/src/game/Game.java
@@ -7,9 +7,14 @@ import system.Avatar;
 public class Game extends GraphicsProgram {
 
 	private static final long serialVersionUID = 7059646278559620203L;
+	public static final int SCREEN_WIDTH = 1920;
+	public static final int SCREEN_HEIGHT = 1080;
+	public static final int xUnit = SCREEN_WIDTH / 12;
+	public static final int yUnit = SCREEN_HEIGHT / 12;
 	private Avatar avatar;
 	
 	public void run() {
+		this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		this.add(new Menu());
 	}
 	

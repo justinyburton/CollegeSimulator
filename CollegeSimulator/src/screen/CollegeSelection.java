3b75e98325ae8c17b3b1f5fa88e458710fbb3b5b
justinyburton <justin.y.burton@gmail.com>
2018-04-28 11:52:49 -0400

    Small changes to fonts in CollegeSelection.

--- CollegeSimulator/src/screen/CollegeSelection.java
+++ CollegeSimulator/src/screen/CollegeSelection.java
@@ -1,6 +1,7 @@
 package screen;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import acm.graphics.GCanvas;
 import acm.graphics.GLabel;
@@ -15,13 +16,20 @@ public class CollegeSelection extends Screen {
 	private GLabel titleLabel;
 	private GLabel SULabel;
 	
+	private Font titleFont;
+	private Font collegeFont;
+	
 	public CollegeSelection(Game game) {
 		
 		super(game);
 		
 		System.out.println("College selection screen generated.");
 		
+		titleFont = new Font(Font.SERIF, Font.BOLD, 20);
+		
 		titleLabel = new GLabel("Select Your College", Game.X_UNIT * 5, Game.Y_UNIT);
+		titleLabel.setFont(titleFont);
+		this.add(titleLabel);
 		
 		SULabel = new GLabel(College.SU.toString(), Game.X_UNIT, Game.Y_UNIT * 2.5);
 		this.add(SULabel);

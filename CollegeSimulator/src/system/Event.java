d7d32a92d5e6068ea7d6269100d60ba908742b1b
Chloe Fang <chloe.fang97@gmail.com>
2018-04-30 16:33:46 -0400

    Implemented takeTime() method

--- CollegeSimulator/src/system/Event.java
+++ CollegeSimulator/src/system/Event.java
@@ -1,5 +1,7 @@
 package system;
 
+import game.Game;
+
 public class Event implements TakesTime {
 
     private int sanity;
@@ -8,14 +10,16 @@ public class Event implements TakesTime {
     private int hunger;
     private int money;
     private int loans;
+    private Game game;
     
-    public final Event SLEEP = new Event(sanity+20, parentalApproval-10, rest+50, hunger+20, money, loans);
-    public final Event STUDY = new Event(sanity-20, parentalApproval+10, rest-20, hunger+30, money, loans);
-    public final Event GO_TO_CLASS = new Event(sanity-15, parentalApproval+10, rest-20, hunger+20, money, loans);
-    public final Event EAT = new Event(sanity+10, parentalApproval-5, rest-10, hunger-30, money-10, loans);
+    public final Event SLEEP = new Event(1, sanity+20, parentalApproval-10, rest+50, hunger+20, money, loans);
+    public final Event STUDY = new Event(1, sanity-20, parentalApproval+10, rest-20, hunger+30, money, loans);
+    public final Event GO_TO_CLASS = new Event(1, sanity-15, parentalApproval+10, rest-20, hunger+20, money, loans);
+    public final Event EAT = new Event(1, sanity+10, parentalApproval-5, rest-10, hunger-30, money-10, loans);
     
-    public Event(int sanity, int parentalApproval, int rest, int hunger, int money, int loans) {
+    public Event(int hour, int sanity, int parentalApproval, int rest, int hunger, int money, int loans) {
         
+        takeTime(0, hour, 0);
         this.sanity = sanity;
         this.parentalApproval = parentalApproval;
         this.rest = rest;
@@ -27,8 +31,7 @@ public class Event implements TakesTime {
 
     @Override
     public void takeTime(int days, int hours, int minutes) {
-        // TODO Auto-generated method stub
-        
+        game.getTime().changeTime(days, hours, minutes);
     }
    
 }

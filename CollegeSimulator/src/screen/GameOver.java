a368b5e1cae546c73a1c0e48a6855061dc696494
justinyburton <justin.y.burton@gmail.com>
2018-04-30 16:59:19 -0400

    Added all files.

---
+++ CollegeSimulator/src/screen/GameOver.java
@@ -0,0 +1,53 @@
+package screen;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import acm.graphics.GLabel;
+import game.Game;
+
+public class GameOver extends Screen {
+	
+	private static final long serialVersionUID = 4025229043284954824L;
+	private final String loss = "Game Over";
+	
+	@SuppressWarnings("unused")
+	private Game game;
+	private JButton newGameButton;
+	private Font titleFont;
+	private GLabel titleLabel;
+	private Font buttonFont;
+	
+	public GameOver(Game game) {
+		super(game);
+
+		//Create the title font
+		titleFont = new Font(Font.SERIF, Font.BOLD, 60);
+		
+		//Create and add the title label
+		titleLabel = new GLabel(loss, Game.X_UNIT * 4.62, Game.Y_UNIT * 4);
+		titleLabel.setFont(titleFont);
+		this.add(titleLabel);
+		
+		//Create the font for the button
+		buttonFont = new Font(Font.SERIF, Font.BOLD, 30);
+		
+		//Create and add new game button
+		newGameButton = new JButton("New Game");
+		newGameButton.setFont(buttonFont);
+		newGameButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.changeScreen(new AvatarBuilder(game));
+			}
+			
+		});
+		newGameButton.setBounds(Game.X_UNIT * 5, Game.Y_UNIT * 5, Game.X_UNIT * 2, Game.Y_UNIT);
+		this.add(newGameButton);
+	}
+
+}
---
+++ CollegeSimulator/src/screen/LibraryScreen.java
@@ -0,0 +1,31 @@
+package screen;
+
+import javax.swing.JButton;
+
+import game.Game;
+
+public class LibraryScreen extends GamePlay {
+
+	public LibraryScreen(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String eventText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JButton[] options() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
